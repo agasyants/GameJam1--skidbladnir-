896e369ffc8278e6447b48fee696aab808e1ca48
Evgeniy <agasyanc@gmail.com>
2025-11-03 22:21:43 +0400

    volume sliders and config save

--- UI/pause.gd
+++ UI/pause.gd
@@ -1,10 +1,25 @@
 extends Control
 
+var volume_bus_name: String = "Music"
+var sfx_bus_name: String = "SFX"
+
 func _ready():
 	process_mode = Node.PROCESS_MODE_ALWAYS
 	$resume_btn.pressed.connect(_on_resume_button_pressed)
 	$restart_btn.pressed.connect(_on_restart_button_pressed)
 	hide()
+
+	# Загрузка сохраненных настроек
+	var volume_data = SaveManager.load_game(volume_bus_name + '.json')
+	var sfx_data = SaveManager.load_game(sfx_bus_name + '.json')
+	$volume_slider.value = 0.8
+	$sfx_slider.value = 0.8
+	
+	if volume_data:
+		$volume_slider.value = volume_data["volume"]
+	if sfx_data:
+		$sfx_slider.value = sfx_data["sfx"]
+
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause"):
@@ -26,3 +41,15 @@ func _on_restart_button_pressed():
 func resume():
 	get_tree().paused = false
 	hide()
+
+
+func _on_volume_slider_value_changed(value: float) -> void:
+	var bus_index = AudioServer.get_bus_index(volume_bus_name)
+	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
+	SaveManager.save_game({"volume": value}, volume_bus_name + '.json')
+
+
+func _on_sfx_slider_value_changed(value: float) -> void:
+	var bus_index = AudioServer.get_bus_index(sfx_bus_name)
+	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
+	SaveManager.save_game({"sfx": value}, sfx_bus_name + '.json')
