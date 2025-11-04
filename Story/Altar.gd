ca4c8a54db078cb19fef04358b0fbcd5dc2c1a78
Evgeniy <agasyanc@gmail.com>
2025-11-04 16:06:47 +0400

    backgrds

--- Story/Altar.gd
+++ Story/Altar.gd
@@ -5,11 +5,17 @@ func _ready():
 
 @export var from: int = 0
 @export var to: int = 1
+@export var backname: String = "Back-02"
 
 @onready var lens: LensManager = get_tree().get_first_node_in_group("manager")
 
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
+		hide()
+		var back = get_parent().get_node(backname)
+		if back:
+			back.show()
 		if body.eye_state == from:
 			body.eye_state = to
-			GameManager.set_checkpoint("altar", body.global_position, body.eye_state, lens.target_lens)
+			GameManager.set_checkpoint("altar", body.global_position, body.eye_state, lens.current_lens)
+			
